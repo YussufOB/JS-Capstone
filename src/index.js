9e8e534849ac7cdbda1ec27498fe15b45256dd8e
Ademola Adebayo <scdk@yahoo.com>
2022-06-03 10:06:49 +0100

    Display list of items on the home page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+import displayData from './modules/loadData.js';
+import addLike from './modules/addLike.js';
+import displayLikes from './modules/viewLikes.js';
+import popup from './modules/commentPopup.js';
+import itemCounter from './modules/itemCounter.js';
+
+const getData = async () => (await fetch('https://api.tvmaze.com/shows/169/seasons')).json();
+const title = document.querySelector('.nav-links a:nth-child(1)');
