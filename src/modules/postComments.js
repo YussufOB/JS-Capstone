65167c00dfdfbca67aaa7ecdb049cf1d42e17f3b
Ademola Adebayo <scdk@yahoo.com>
2022-06-03 11:18:47 +0100

    Display number of likes

--- src/modules/postComments.js
+++ src/modules/postComments.js
@@ -1,4 +1,4 @@
-import addComment from './addComment.js';
+import addComment from './addComments.js';
 
 export default async ({ id, username, comment }) => {
   const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/mU67F6D8b0eq3yf1Ab3T/comments';
